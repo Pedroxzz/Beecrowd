129c24585b3ca60cc4ea88b66cfff03fe620684b
João Pedro <joaopedroicm2117@gmail.com>
2022-07-13 23:59:24 -0300

    Exercicios do 00 até 05

---
+++ 1_Iniciante/Java/ex1000.java
@@ -0,0 +1,13 @@
+/*
+-Entrada- 
+Este problema não possui nenhuma entrada.
+  
+-Saída-
+Você deve imprimir a mensagem "Hello World!" e em seguida o final de linha, conforme o exemplo abaixo.
+ */
+
+public class ex1000 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
